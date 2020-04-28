08eaf26f06fe294f267b959a159288d955128ea7
Shane Ashby <ashby20s@gmail.com>
2020-04-28 12:41:56 -0600

    Adding new links and pages

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,6 +2,27 @@ class PostController < ApplicationController
   def index
   end
 
+  def memberships
+  end
+
   def about
   end
+
+  def programs
+  end
+  
+  def fitness
+  end
+
+  def community
+  end
+
+  def personaltraining
+  end
+
+  def contact
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root to: 'post#index'
 
+  get 'post/memberships'
   get 'post/about'
+  get 'post/programs'
+  get 'post/fitness'
+  get 'post/contact'
+  get 'post/personaltraining'
+  get 'post/community'
+
 end
